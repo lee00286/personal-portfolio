d3d4278b2f99cdb04915679c09046ee065586087
lee00286 <33945159+lee00286@users.noreply.github.com>
2023-12-09 14:26:41 -0500

    feat: added meta robots tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,20 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Portfolio',
-  description: 'Portfolio'
+  description: 'Portfolio',
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1
+    }
+  }
 };
 
 export default function RootLayout({
