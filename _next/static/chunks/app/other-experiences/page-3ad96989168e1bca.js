d83a55c3e724c9dcdfec5aeef3b8dd0853942c42
lee00286 <lee00286@naver.com>
2023-12-10 23:09:37 -0500

    deploy to gh-pages

---
+++ _next/static/chunks/app/other-experiences/page-3ad96989168e1bca.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[425],{},function(n){n.O(0,[298,561,253,961,744],function(){return n(n.s=7561)}),_N_E=n.O()}]);
--- config/config.ts
+++
@@ -1,4 +0,0 @@
-export const prefix: string =
-  process.env.NODE_ENV === 'production'
-    ? 'https://lee00286.github.io/portfolio'
-    : '';
